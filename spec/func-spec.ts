ab42222265953675850659c672178a15c704ac73
Andrew Arnott <andrewarnott@gmail.com>
2020-07-30 11:50:42 -0600

    Add test for a valid (but mostly ignored) message

--- spec/func-spec.ts
+++ spec/func-spec.ts
@@ -3,13 +3,39 @@
 import func from "../GitHubHook/index"
 import { Context, HttpRequest } from "@azure/functions"
 import mockedEnv, { RestoreFn } from "mocked-env"
+import { Webhooks } from "@octokit/webhooks"
+import fs from "fs"
+
+interface IMockMessage {
+	eventType: ExpectedEventTypes
+	payload: {}
+}
+
+function ParseMockMessage(name: string): IMockMessage {
+	return {
+		eventType: name.split(".")[0] as ExpectedEventTypes,
+		payload: JSON.parse(fs.readFileSync(`${__dirname}/../testAssets/${name}`).toString()),
+	}
+}
+
+const MockMessages = {
+	pull_request: {
+		closed: ParseMockMessage("pull_request.closed.json"),
+	},
+}
+
+const mockWebhookSecret = "some secret"
 
 let restore: RestoreFn
 beforeEach(() => {
 	restore = mockedEnv({
 		GitHubAppId: "1234",
-		GitHubWebhookSecret: "some secret",
+		GitHubWebhookSecret: mockWebhookSecret,
 	})
+})
+
+const webhooks = new Webhooks({
+	secret: mockWebhookSecret,
 })
 
 afterEach(() => {
@@ -18,21 +44,29 @@ afterEach(() => {
 
 describe("Azure Function", () => {
 	it("rejects bad signature", async () => {
-		const context: Context = NewContext(NewHttpRequest())
+		const context: Context = NewContext(NewHttpRequest({ eventType: "pull_request", payload: {} }))
+		context.req!.headers["x-hub-signature"] = "bad sig"
 		await expect(func(context, context.req)).rejects.toThrowError("signature does not match event payload and secret")
+	})
+
+	it("does nothing with pull_request.closed", async () => {
+		const context: Context = NewContext(NewHttpRequest(MockMessages.pull_request.closed))
+		await func(context, context.req)
 	})
 })
 
-function NewHttpRequest(): HttpRequest {
+type ExpectedEventTypes = "pull_request" | "pull_request_review" | "check_suite"
+
+function NewHttpRequest(mockMessage: IMockMessage): HttpRequest {
 	return {
 		method: "POST",
 		url: "mock",
 		headers: {
 			"x-github-delivery": "mock-guid",
-			"x-github-event": "mock-event",
-			"x-hub-signature": "signature",
+			"x-github-event": mockMessage.eventType,
+			"x-hub-signature": webhooks.sign(mockMessage.payload),
 		},
-		body: JSON.stringify({}),
+		body: mockMessage.payload,
 		query: {},
 		params: {},
 	}
